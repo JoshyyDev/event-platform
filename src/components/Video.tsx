02eb656e988014303b04ec3af467f8b7e800907a
JoshyyDev <jrcbr321@gmail.com>
2022-06-24 16:40:50 -0300

    Finalizando routers

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,15 +1,61 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css'
 
-export function Video() {
+interface VideoProps {
+    lessonSlug: string;
+}
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+    query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+        title
+        videoId
+        description
+        teacher {
+        bio
+        avatarURL
+        }
+    }
+}
+`
+
+interface GetLessonBySlugResponse {
+    lesson: {
+        title: string;
+        videoId: string;
+        description: string;
+        teacher: {
+            bio: string;
+            avatarURL: string;
+            name: string;
+        }
+    }
+}
+
+export function Video(props: VideoProps) {
+    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY , {
+        variables: {
+            slug: props.lessonSlug,
+        }
+    })
+
+    if (!data) {
+        return (
+            <div className="flex-1">
+                <p>Carregando...</p>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1">
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId="Dv7gLpW91DM" />
+                        <Youtube videoId={data.lesson.videoId} />
                         <DefaultUi />
                     </Player>
                 </div>
@@ -19,22 +65,22 @@ export function Video() {
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold">
-                            Why Programming Is Important?
+                        {data.lesson.title}
                         </h1>
                         <p className="mt-4 text-gray-200 leading-relaxed">
-                            This is a code.org video that explains everything we should know and do. I do not own this video. I shared it just to spread the idea for non-profit. Thank you and happy coding.
+                        {data.lesson.description}
                         </p>
 
                         <div className="flex item-center gap-4 mt-6">
                             <img
                             className="h-16 w-16 rounded-full border-2 border-blue-500"
-                            src="https://github.com/JoshyyDev.png" 
+                            src={data.lesson.teacher.avatarURL}
                             alt="" 
                             />
 
                             <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">Josué Cortez</strong>
-                                <span className="text-gray-200 text-sm block">Programador</span>
+                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
                             </div>
                         </div>
                     </div>
